0d6658ba2fc7c33539171085816d6aa1956af784
Matheus Tardivo <matheustardivo@gmail.com>
2012-02-24 17:10:09 -0200

    Added notification status

--- lib/pagseguro_client/notification.rb
+++ lib/pagseguro_client/notification.rb
@@ -10,6 +10,15 @@ module PagseguroClient
       5 => :oi_paggo
     }
     
+    STATUS = {
+      1 => :pending,
+      2 => :verifying,
+      3 => :approved,
+      4 => :available,
+      6 => :refunded,
+      7 => :canceled
+    }
+    
     attr_accessor :code, :order_id, :order_code, :status, :payment_method
     
     def initialize(attributes = {})
@@ -31,14 +40,14 @@ module PagseguroClient
       doc = Nokogiri::XML(response.body)
       order_id = doc.xpath("//reference").text
       order_code = doc.xpath("//code").text
-      status = doc.xpath("//status").text
+      status = doc.xpath("//status").text.to_i
       payment_method = doc.xpath("//paymentMethod/type").text.to_i
       
       Notification.new(
         code: code, 
         order_id: order_id, 
         order_code: order_code, 
-        status: status, 
+        status: STATUS[status], 
         payment_method: PAYMENT_METHOD[payment_method]
       )
     end
