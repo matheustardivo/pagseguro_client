fa4730d6f495fee4c8125c6c6a077d1e5a5c723a
Matheus Tardivo <matheustardivo@gmail.com>
2012-05-05 20:17:10 -0300

    Refatoração geral no spec de orders

--- spec/pagseguro_client/order_spec.rb
+++ spec/pagseguro_client/order_spec.rb
@@ -1,34 +1,42 @@
 require "spec_helper"
 
-describe PagseguroClient::Order do
-  
-  it "should set order id when instantiating object" do
-    order = PagseguroClient::Order.new("ABCDEF")
-    order.id.should == "ABCDEF"
+module PagseguroClient
+  describe Order do
+
+    context "order with id" do
+      subject {
+        Order.new("ABCDEF")
+      }
+
+      its(:id) { should == "ABCDEF" }
+    end
+
+    context "order with attributes" do
+      subject {
+        order = Order.new("John Doe")
+        order.add(id: "1", description: "Description", amount: 199)
+        order
+      }
+
+      it "should add product" do
+        subject.products.should_not be_empty
+        subject.products.size.should == 1
+        subject.products.first[:id].should == "1"
+      end
+
+      it "should set order product data" do
+        subject.add(id: "2", description: "Description", amount: 199)
+        subject.add(id: "3", description: "Description", amount: 199)
+
+        data = subject.data
+        data.keys.include?("itemId1").should be_true
+        data.keys.include?("itemId2").should be_true
+        data.keys.include?("itemId3").should be_true
+
+        data["itemId1"].should == "1"
+        data["itemId2"].should == "2"
+        data["itemId3"].should == "3"
+      end
+    end
   end
-  
-  it "should add product" do
-    order = PagseguroClient::Order.new("John Doe")
-    order.add(id: "1", description: "Description", amount: 199)
-    order.products.should_not be_empty
-    order.products.size.should == 1
-    order.products.first[:id].should == "1"
-  end
-  
-  it "should set order product data" do
-    order = PagseguroClient::Order.new("John Doe")
-    order.add(id: "1", description: "Description", amount: 199)
-    order.add(id: "2", description: "Description", amount: 199)
-    order.add(id: "3", description: "Description", amount: 199)
-    
-    data = order.data
-    data.keys.include?("itemId1").should be_true
-    data.keys.include?("itemId2").should be_true
-    data.keys.include?("itemId3").should be_true
-    
-    data["itemId1"].should == "1"
-    data["itemId2"].should == "2"
-    data["itemId3"].should == "3"
-  end
-  
 end
