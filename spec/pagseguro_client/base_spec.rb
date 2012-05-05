aa025ac9b924d30a217dac48b96e5c9f08cdc869
Matheus Tardivo <matheustardivo@gmail.com>
2012-05-05 20:19:16 -0300

    Spec para o módulo base do PagseguroClient.

---
+++ spec/pagseguro_client/base_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe "Base" do
+  it "should return the payment url" do
+    PagseguroClient.payment_url("XPTO").should == "#{PagseguroClient.base_url}/v2/checkout/payment.html?code=XPTO"
+  end
+
+  it "should return the notification url" do
+    PagseguroClient.notification_url("XPTO").should == "#{PagseguroClient.base_url}/v2/transactions/notifications/XPTO"
+  end
+end
